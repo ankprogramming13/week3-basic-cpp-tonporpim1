a4de49eba3f7931d82f26532ba21a27e47b42195
Unknown <pimlapatpat@cvc-cha.ac.th>
2017-11-21 17:37:23 +0700

    ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << " Mr." << "Pimlapat Patsadu" << endl;
+  cout << "Tonpor" << endl;
+  cout << "male" << endl;
+  cout << "5 mar 1998" << endl;
+  cout << "warch a mavie" << endl;
+  cout << "single";
   return 0;
 }
