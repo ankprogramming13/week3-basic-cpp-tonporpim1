d2a5e33f0eb4d9f3bee52bb503ed470a056df4ba
Unknown <pimlapatpat@cvc-cha.ac.th>
2017-11-24 14:02:42 +0700

    ข้อที่ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -6,7 +6,10 @@ int main() {
 
   const float ip = 3.14315926536;
   float r;
+  float h; = 15;
   cin >> r;
-  cout << "Area :"<< pi * r * r;
+  cout << "Area : " << pi * r * r;
+  cout << "Round: " << pi * 2 * r;
+  cout << "volume:" << pi * r * r *h;
   return 0;
 }
